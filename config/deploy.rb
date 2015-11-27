2e67d01981d3839d65868f471286a0f9f7077fdd
Cicuta364 <patricio.campos.u@gmail.com>
2015-11-26 21:40:36 -0300

    Add gem missing

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :rvm_type
+set :rvm_type, :user
 set :rvm_ruby_version, '2.2.3'
 
 set :application, 'blogo'
-set :deploy_to, "/home/ubuntu/www/#{fetch(application)}"
+set :deploy_to, "/home/ubuntu/www/#{fetch(:application)}"
 set :scm, :git
-set :repo_url, 'https://github.com/Cicuta364/Blogo.git'
+set :repo_url, 'git@github.com:cicuta364/Blogo.git'
 set :branch, 'master'
 
 set :linked_files, %w{config/database.yml config/secrets.yml .env}
